7efb2ced5c48b68e9e3c14935cd2f278bc97e51e
Kim-yrolG <kky9429@gmail.com>
2025-02-10 13:14:10 +0900

    [Add] 증강체 데이터 추가

---
+++ Assets/CustomFolder - Augment/AugmentDataImport/DataManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DataManager : MonoBehaviour
+{
+	static DataManager instance;
+	
+	public PlayerLevelData playerLevelData;
+	public MonsterLevelData monsterLevelData;
+	
+	public static DataManager Instance
+	{
+		get
+		{
+			return instance;
+		}
+	}
+
+	void Awake()
+	{
+		if ( instance == null )
+		{
+            Destroy(gameObject);	
+			instance = this;
+			//자료 로딩
+			playerLevelData = Resources.Load ("Data/PlayerLevelData") as PlayerLevelData;
+			monsterLevelData = Resources.Load ("Data/MonsterLevelData") as MonsterLevelData;
+		}
+		else
+		{
+			Destroy(gameObject);
+		}
+	}
+	
+	void Update()
+	{
+		if ( Input.GetKeyDown(KeyCode.Escape))
+		{
+			Application.Quit ();
+		}
+	}
+}
---
+++ Assets/CustomFolder - Augment/AugmentDataImport/PlayerLevelData.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerLevelData : ScriptableObject
+{
+	[System.Serializable]
+	public class Attribute
+	{
+		public int level;
+		public int maxHP;
+		public float baseAttack;
+		public int reqExp;
+		public float moveSpeed;
+		public float turnSpeed;
+		public float attackRange;
+	}
+
+	public List<Attribute> list = new List<Attribute>();
+}
